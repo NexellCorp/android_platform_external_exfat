854c593bf1522cc6c90f41b2aafa7bcc7aea907d
allan park <allan.park@nexell.co.kr>
2020-02-19 13:59:27 +0900

    external_exfat: change file path external to vendor
    This patch change file path external to vendor.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>

--- fsck/Android.mk
+++ fsck/Android.mk
@@ -6,8 +6,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
 LOCAL_SRC_FILES =  main.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-					external/exfat/libexfat \
-					external/fuse/include
+	vendor/nexell/external/exfat/libexfat \
+	vendor/nexell/external/fuse/include
 LOCAL_STATIC_LIBRARIES := libexfat_static
 include $(BUILD_EXECUTABLE)
 
@@ -17,8 +17,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64 -Dmain=fsck_exfat_main
 LOCAL_SRC_FILES =  main.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-					external/exfat/libexfat \
-					external/fuse/include
+	vendor/nexell/external/exfat/libexfat \
+	vendor/nexell/external/fuse/include
 LOCAL_STATIC_LIBRARIES := libfuse_static libexfat_static
 include $(BUILD_STATIC_LIBRARY)
 
